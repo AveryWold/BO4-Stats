448b9d19a21c92ac498375a78e3d8cf5792ecd87
Avery Wold <mrstinky82@gmail.com>
2019-02-07 09:22:16 -0600

    code cleanup

--- src/Store.js
+++ src/Store.js
@@ -3,6 +3,7 @@ import Reducers from './Reducers';
 import InitialState from './InitialState';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import React from 'react';
 
 const Store = createStore(Reducers, InitialState, applyMiddleware(thunk, createLogger()));
 
